2eadaaa702383d50bd3f93cd791822a0cc63945e
etigoshun <k021g1375@g.neec.ac.jp>
2023-02-20 14:12:24 +0900

    Enemy.cpp.hの追加

---
+++ Enemy.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "Model.h"
+#include "WorldTransform.h"
+#include "Input.h"
+#include "Model.h"
+#include "DebugText.h"
+#include "Function.h"
+
+class Enemy
+{
+public:		//メンバ関数
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="texturehandle">テクスチャハンドル</param>
+	void Initialize(Model* model);
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(ViewProjection& viewProjection);
+
+	//ワールド座標を取得
+	Vector3 GetWorldPosition();
+
+	//衝突を検出したら呼び出されるコールバック関数
+	void OnCollision();
+
+	float GetRadius() { return radius; }
+
+private:
+	//半径
+	const float radius = 1.0f;
+
+private:	//メンバ変数
+
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+
+	//モデル
+	Model* model_ = nullptr;
+
+	//入力処理するため
+	Input* input_ = nullptr;
+
+	//デバッグテキスト
+	DebugText* debugText_ = nullptr;
+
+	//ファンクション
+	MyFunc myFunc_;
+
+	//切り替えフラグ
+	bool LightFlag = false;
+	bool LeftFlag = false;
+};
+
